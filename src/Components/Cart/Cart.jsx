09a1f9e187d628cc3fa674b2fa36ac81a97339b6
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-03-29 11:20:20 +0600

    fontowsome add

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,16 +4,19 @@
   
     let totalPrice = 0;
     let totalShipping =0;
+    let quantity =0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price
-        totalShipping= totalShipping+product.shipping
+        product.quantity= product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity ;
+        totalShipping= totalShipping+product.shipping ;
+        quantity= quantity+product.quantity ;
     }
     const tax= totalPrice*7/100;
     const grandTotal = totalPrice+totalShipping+tax
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected item: {cart.length}</p>
+            <p>Selected item: {quantity}</p>
             <p>Total Price:$ {totalPrice.toFixed(2)}</p>
             <p>Total Shipping:$ {totalShipping.toFixed(2)}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/Components/Shop/Product.jsx
+++ src/Components/Shop/Product.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+    
     
    const handleAddToCart=  props.handleAddToCart
     const {img,name,price,seller,ratings}=props.product
@@ -14,7 +17,7 @@ const Product = (props) => {
                     <p className='text-xl font-bold '>Manufacturer: ${seller}</p>
                     <p className='text-xl font-bold '>Rating: ${ratings} Star</p>
                     <div className="card-actions mt-10 w-full">
-                        <button onClick={()=>handleAddToCart(props.product)} className="btn btn-primary w-full">Add to Cart </button>
+                        <button onClick={()=>handleAddToCart(props.product)} className="btn btn-primary w-full">Add to Cart <FontAwesomeIcon  icon= {faShoppingCart} /></button>
                     </div>
                 </div>
             </div>
